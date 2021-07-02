8208d68606c2abeec6f4af63a89e70f12a7d8374
Jordi <jordi@constantine.local>
2021-07-02 18:07:03 +0200

    FFT step 1.

--- fft_decimation_in_time.py
+++ fft_decimation_in_time.py
@@ -14,3 +14,15 @@ w = 0.5 * (1 - np.cos(2*np.pi*np.arange(L)/N))
 # Windowed signal
 xw = x * w
 
+# Wkn
+Wkn = np.exp(-1j * 2*np.pi * np.arange(4)/L)
+
+# Step 1 combine first with second half of the sequence
+X1 = np.zeros(L)
+X1[0:4] = xw[0:4] + xw[4:]
+X1[4:8] = xw[0:4] - xw[4:]
+
+print('Pass 1a', X1)
+# Multiply second half by Wkn
+X1 = X1 * np.hstack((np.ones_like(Wkn), Wkn))
+print('Pass 1b', X1)
